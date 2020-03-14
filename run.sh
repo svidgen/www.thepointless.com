303c8404486ae90797f088d97d781bb92680aab2
Jon Wire <iambipedal@gmail.com>
2020-03-14 16:59:42 -0500

    added http.php for compatbility with web-fleet-setup

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-php -S 0.0.0.0:8890 -t . router.php &
-php -S 0.0.0.0:8891 -t . router.php &
-php -S 0.0.0.0:8892 -t . router.php &
-php -S 0.0.0.0:8893 -t . router.php &
-php -S 0.0.0.0:8894 -t . router.php &
-php -S 0.0.0.0:8895 -t . router.php &
-php -S 0.0.0.0:8896 -t . router.php &
-php -S 0.0.0.0:8897 -t . router.php &
+php -S 0.0.0.0:8890 -t . http.php &
+php -S 0.0.0.0:8891 -t . http.php &
+php -S 0.0.0.0:8892 -t . http.php &
+php -S 0.0.0.0:8893 -t . http.php &
+php -S 0.0.0.0:8894 -t . http.php &
+php -S 0.0.0.0:8895 -t . http.php &
+php -S 0.0.0.0:8896 -t . http.php &
+php -S 0.0.0.0:8897 -t . http.php &
 sleep 0.3
 php load_balance.php 8890 8
 kill -- -$$
