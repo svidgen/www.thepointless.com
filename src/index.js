332a4291c09dc9efa3272ebaec704433ac5c6c2c
Jon Wire <iambipedal@gmail.com>
2021-06-17 21:39:52 -0500

    added wirejs-dom, added share component to prove it's working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const GoogleAds = require('ex-gratia/google');
 
+require('./components/share.js');
+
 // no ads at "home".
 if (!location.hostname.match(/^localhost|127\.0\.0\.1$/)) {
 	new GoogleAds().install();
