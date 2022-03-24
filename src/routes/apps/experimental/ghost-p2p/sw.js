907561174431aba3c4d312cdfe96fc52131e2f06
Jonathan Wire <iambipedal@gmail.com>
2022-03-23 22:00:31 -0500

    ghost p2p pwa init with svg icon

---
+++ src/routes/apps/experimental/ghost-p2p/sw.js
@@ -0,0 +1,55 @@
+const BUILD_ID = require('/src/build_id.json');
+
+const CACHE_NAME = 'experimental-p2p-push-' + BUILD_ID;
+
+const urlsToCache = [
+	'index.html',
+	'manifest.json',
+	'js/index.js'
+];
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(function(cache) {
+			console.log('Opened cache');
+			// return cache.addAll(urlsToCache);
+			return Promise.all(urlsToCache.map(function(url) {
+				return fetch(url, {
+					cache: 'no-cache',
+					credentials: 'include'
+				}).then(function(response) {
+					return cache.put(url, response);
+				});
+			}));
+		})
+	);
+});
+
+
+self.addEventListener('activate', function(event) {
+	var cacheWhitelist = [CACHE_NAME];
+	event.waitUntil(
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.map(function(cacheName) {
+					if (cacheWhitelist.indexOf(cacheName) === -1) {
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+	);
+});
+
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			// Cache hit - return response
+			if (response) {
+				return response;
+			}
+			return fetch(event.request);
+		})
+	);
+});
--- src/routes/words/index.md
+++ src/routes/words/index.md
@@ -6,11 +6,11 @@ Words come in all shapes and sizes. They say many different things. At times, yo
 
 Not that I'm *not* a loony. But, I do ask you to refrain from swinging such an accusation about *willy-nilly*. You might find such accusations smacking perfectly sane bystanders in the face. And we shan't have that. Shan we?
 
-* [Serious words](./serious/index.html)
-	<br />You have been warned.
+### [Serious words](./serious/index.html)
+> You have been warned.
 
-* [Silly words](./silly/shorts/index.html)
-	<br />Words like zabing and bazoot would be classified as such, amongst other words, like orangutan.
+### [Silly words](./silly/shorts/index.html)
+> Words like zabing and bazoot would be classified as such, amongst other words, like orangutan.
 
-* [Unclassified words](./unclassified/index.html)
-	<br />These words may or may not be silly. (Like zebra.) And I prefer them to be ambiguous. (You can let me know if you think they're silly or serious on Twitter, if you like.)
+### [Unclassified words](./unclassified/index.html)
+> These words may or may not be silly. (Like zebra.) And I prefer them to be ambiguous. (You can let me know if you think they're silly or serious on Twitter, if you like.)
