69b4c650973ef94e22bc7df33b69b62501828562
Jonathan Wire <iambipedal@gmail.com>
2022-03-29 22:52:27 -0500

    just updating logging on p2p poc to differentiate events

--- src/routes/apps/experimental/ghost-p2p/js/index.js
+++ src/routes/apps/experimental/ghost-p2p/js/index.js
@@ -5,46 +5,49 @@ pc = new RTCPeerConnection({
 		{ urls: 'stun:stun3.l.google.com:19302' }
 	]
 });
-dc = pc.createDataChannel('test', {reliable: true})
+dc = pc.createDataChannel('test')
+
+dc.addEventListener("open", (event) => {
+	// beginTransmission(dataChannel);
+	console.log('open', event);
+});
 
 pc.onicecandidate = (event) => {
 	if (event.candidate) {
-	  // sendCandidateToRemotePeer(event.candidate)
-	  console.log(event);
+		// sendCandidateToRemotePeer(event.candidate)
+		console.log('event', event);
 	} else {
-	  /* there are no more candidates coming during this negotiation */
+		/* there are no more candidates coming during this negotiation */
 	}
-  }
+};
 
-  pc.createOffer().then(function(offer) {
-	  console.log(offer);
-    return pc.setLocalDescription(offer);
-  })
-  .then(function() {
-    console.log({
-    //   name: myUsername,
-    //   target: targetUsername,
-    //   type: "video-offer",
+pc.createOffer().then(function (offer) {
+	console.log('offer', offer);
+	return pc.setLocalDescription(offer);
+}).then(function () {
+	console.log({
+		//   name: myUsername,
+		//   target: targetUsername,
+		//   type: "video-offer",
 		type: 'data-offer',
-      sdp: pc.localDescription
-    });
-  })
-  .catch(function(reason) {
-	  console.log(reason);
-    // An error occurred, so handle the failure to connect
-  });
-
-  pc.addEventListener("negotiationneeded", ev => {
-	pc.createOffer()
-	.then(offer => pc.setLocalDescription(offer))
-	.then(() => sendSignalingMessage({
-	  type: "video-offer",
-	  sdp: pc.localDescription
-	}))
-	.catch(err => {
-	  /* handle error */
+		sdp: pc.localDescription
 	});
-  }, false);
+}).catch(function (reason) {
+	console.log('reason', reason);
+	// An error occurred, so handle the failure to connect
+});
+
+//   pc.addEventListener("negotiationneeded", ev => {
+// 	pc.createOffer()
+// 	.then(offer => pc.setLocalDescription(offer))
+// 	.then(() => sendSignalingMessage({
+// 	  type: "video-offer",
+// 	  sdp: pc.localDescription
+// 	}))
+// 	.catch(err => {
+// 	  /* handle error */
+// 	});
+//   }, false);
 
 // pc.
 
@@ -52,7 +55,7 @@ pc.onicecandidate = (event) => {
 // 	if (!(self.Notification && self.Notification.permission === 'granted')) {
 // 		return;
 // 	  }
-	
+
 // 	  var data = {};
 // 	  if (event.data) {
 // 		data = event.data.json();
@@ -60,13 +63,13 @@ pc.onicecandidate = (event) => {
 // 	  var title = data.title || "Something Has Happened";
 // 	  var message = data.message || "Here's something you might want to check out.";
 // 	  var icon = "images/new-notification.png";
-	
+
 // 	  var notification = new Notification(title, {
 // 		body: message,
 // 		tag: 'simple-push-demo-notification',
 // 		icon: icon
 // 	  });
-	
+
 // 	  notification.addEventListener('click', function() {
 // 		if (clients.openWindow) {
 // 		  clients.openWindow('https://example.blog.com/2015/03/04/something-new.html');
