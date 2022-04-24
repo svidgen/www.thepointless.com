de7d8609efaceae99f31b72c82659fb5745176fd
Jonathan Wire <iambipedal@gmail.com>
2022-04-23 19:52:14 -0500

    added mermaid parsing; added diagram to contribute page

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
-const CopyWebpackPlugin = require('copy-webpack-plugin'); 
+const { exec } = require('child_process');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const marked = require('marked');
+const { JSDOM } = require('jsdom');
+
 const sitemap = require('./src/sitemap');
 
 // https://marked.js.org/using_advanced
 marked.setOptions({
-	highlight: function(code, lang) {
+	highlight: function (code, lang) {
 		const highlighter = require('highlight.js');
 		const language = highlighter.getLanguage(lang) ? lang : 'plaintext';
 		return highlighter.highlight(code, { language }).value;
@@ -25,8 +28,8 @@ fs.writeFileSync('./src/build_id.json', JSON.stringify(BUILD_ID.toString()));
 // TODO: consider whether using the front-end framework for SSG would be safe,
 // and intuitive, rather than having two completely separate rendering modes.
 
-function distPath({subpathOut = '', subpathIn = ''} = {}) {
-	return function({context, absoluteFilename}) {
+function distPath({ subpathOut = '', subpathIn = '' } = {}) {
+	return function ({ context, absoluteFilename }) {
 		const prefixIn = path.resolve(context, subpathIn);
 		const prefixOut = path.resolve(context, 'dist', subpathOut);
 		const relativeName = path.join('./', absoluteFilename.slice(prefixIn.toString().length));
@@ -47,75 +50,112 @@ const CollectLayouts = {
 	}
 };
 
-const SSG = {
-	transformer: (content, _path) => {
+async function mermaid(text) {
+	const tempbase = `__mermaid_temp_${new Date().getTime()}`;
+	const tempInput = `${tempbase}.txt`;
+	const tempOutput = `${tempbase}.svg`;
 
-		// TODO: move to a directives module.
-		let _meta = {};
-		function meta(o) {
-			_meta = o;
-			return '';
-		}
-
-		let body;
-		try {
-			if (_path.endsWith('.md')) {
-				let isInCodeBlock = false;
-				const escapedMarkdown = content.toString().split(/\n/)
-					.reduce((lines, line) => {
-						if (isInCodeBlock) {
-							lines[lines.length-1] += "\n" + line;
-						} else {
-							lines.push(line);
-						}
-						if (line.startsWith('```')) {
-							isInCodeBlock = !isInCodeBlock;
-						}
-						return lines;
-					}, [])
-					.map(l => l.trim()).join('\n')
-					.replace(/(``+)/g, m => Array(m.length).fill('\\`').join(''))
-				;
-				const bodyMarkdown = eval('`' + escapedMarkdown + '`');
-				body = marked(bodyMarkdown);
-			} else {
-				body = eval('`' + content + '`');
+	fs.writeFileSync(tempInput, text);
+	await new Promise((resolve, reject) => {
+		exec(`yarn mmdc -i ${tempInput} -o ${tempOutput} -b transparent`, (err) => {
+			if (err) {
+				reject(err);
 			}
-		} catch (err) {
-			console.error(`Could not parse page ${_path}`, err);
-			throw err;
-		}
+			resolve();
+		});
+	});
 
-		const metatags = Object.entries(_meta).map(([tag, content]) => {
-			tag = tag.replace(/"/g, '&quot;');
-			content = content.replace(/"/g, '&quot;');
-			return `<meta name="${tag}" content="${content}" />`;
-		}).join('\n');
-		let title = _meta.title;
+	const svg = fs.readFileSync(tempOutput);
+	fs.unlinkSync(tempInput)
+	fs.unlinkSync(tempOutput);
 
-		// apply no layout if the document has already provided the
-		// overarching html structure.
-		if (!_meta.layout && body && (
-			String(body).startsWith('<!doctype html>')
-			|| String(body).startsWith('<html'))
-		) {
-			return body;
-		}
+	return svg;
+};
 
-		const layoutPath = path.join(
-			'src',
-			'layouts',
-			(_meta.layout || 'default')
-		) + '.html';
-		const layout = layouts[layoutPath];
-		
-		try {
-			return eval('`' + layout + '`');
-		} catch (err) {
-			console.error(`Could not parse layout ${layoutPath}`, err);
-			throw err;
-		}
+const SSG = {
+	transformer: async (content, _path) => {
+// const SSG = async (content, _path) => {
+	// TODO: move to a directives module.
+	let _meta = {};
+	function meta(o) {
+		_meta = o;
+		return '';
 	}
+
+	let body;
+	try {
+		if (_path.endsWith('.md')) {
+			let isInCodeBlock = false;
+			const escapedMarkdown = content.toString().split(/\n/)
+				.reduce((lines, line) => {
+					if (isInCodeBlock) {
+						lines[lines.length - 1] += "\n" + line;
+					} else {
+						lines.push(line);
+					}
+					if (line.startsWith('```')) {
+						isInCodeBlock = !isInCodeBlock;
+					}
+					return lines;
+				}, [])
+				.map(l => l.trim()).join('\n')
+				.replace(/(``+)/g, m => Array(m.length).fill('\\`').join(''))
+				;
+			const bodyMarkdown = eval('`' + escapedMarkdown + '`');
+			const prebody = marked(bodyMarkdown);
+			const { window } = new JSDOM(prebody, { querySelectorAll: true });
+
+			if (window && window.document && window.document.body) {
+				await Promise.all(
+					[...window.document.body
+						.querySelectorAll('.language-mermaid')
+					].map(async mdNode => {
+						const svg = await mermaid(mdNode.textContent);
+						mdNode.parentNode.innerHTML = svg;
+					})
+				);
+				body = window.document.body.innerHTML;
+			} else {
+				body = prebody;
+			}
+		} else {
+			body = eval('`' + content + '`');
+		}
+	} catch (err) {
+		console.error(`Could not parse page ${_path}`, err);
+		throw err;
+	}
+
+	const metatags = Object.entries(_meta).map(([tag, content]) => {
+		tag = tag.replace(/"/g, '&quot;');
+		content = content.replace(/"/g, '&quot;');
+		return `<meta name="${tag}" content="${content}" />`;
+	}).join('\n');
+	let title = _meta.title;
+
+	// apply no layout if the document has already provided the
+	// overarching html structure.
+	if (!_meta.layout && body && (
+		String(body).startsWith('<!doctype html>')
+		|| String(body).startsWith('<html'))
+	) {
+		return body;
+	}
+
+	const layoutPath = path.join(
+		'src',
+		'layouts',
+		(_meta.layout || 'default')
+	) + '.html';
+	const layout = layouts[layoutPath];
+
+	try {
+		return eval('`' + layout + '`');
+	} catch (err) {
+		console.error(`Could not parse layout ${layoutPath}`, err);
+		throw err;
+	}
+}
 };
 
 module.exports = (env, argv) => {
@@ -127,7 +167,7 @@ module.exports = (env, argv) => {
 	const sources = ['./src/index.js']
 		.concat(glob.sync('./src/routes/**/*.js'))
 		.concat(glob.sync('./src/layouts/**/*.js'))
-	;
+		;
 
 	const entry = sources.reduce((files, path) => {
 		if (path.match(/src\/routes/)) {
@@ -212,10 +252,12 @@ module.exports = (env, argv) => {
 					test: /\.css$/,
 					use: [
 						"style-loader",
-						{ loader: "css-loader", options: {
-							// don't try to require() url assets
-							url: false
-						} }
+						{
+							loader: "css-loader", options: {
+								// don't try to require() url assets
+								url: false
+							}
+						}
 					]
 				},
 				{
