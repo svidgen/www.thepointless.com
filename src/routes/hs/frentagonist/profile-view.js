188722254252350fcf1802190a879c5d705e5b01
Jonathan Wire <iambipedal@gmail.com>
2022-05-20 21:49:47 -0500

    removed fb share, not showing emoji, and removed profile CTA, as it's over 160 chars

--- src/routes/hs/frentagonist/profile-view.js
+++ src/routes/hs/frentagonist/profile-view.js
@@ -8,10 +8,9 @@ const markup = `<ft:profileview>
 	<div data-id='actions'>
 		<tpdc:share
 			data-id='share'
-			methods='facebook,twitter,email,copy,qr,native'
+			methods='twitter,email,copy,qr,native'
 		><div data-id='header' style='color: darkgreen; font-weight: bold;'>
-				Share or post in your profile to <br />
-				separate the friends from the foes.
+				Share to separate the friends from the foes.
 			</div>
 		</tpdc:share>
 	</div>
