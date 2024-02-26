60cee20056c7f33c4329b6ec81129804397d49ea
Jonathan Wire <iambipedal@gmail.com>
2024-02-25 22:30:46 -0600

    news update, red dot beard biker gang

--- src/news.js
+++ src/news.js
@@ -1,14 +1,42 @@
 const news = [
 	{
+		title: "What's taking so long? And where is my fungible dot token!?",
+		link: "https://www.thepointless.com/#whats-taking-so-long-and-where-is-my-fungible-dot-token",
+		description: `It isn't easy when a dot-com as reputable and beloved as the one and only pointless dot-com falls short of its dedicated patrons' expecations. And indeed, we have fallen short. As you can plainly see, our last public update was a whopping eight months ago when we promised semi-fungible red dot tokens. "And where are they?" you ask.
+		
+		And boy, are you ever right to ask. But boy, do we ever have some answer. Read it over at thepointless.com.`.replace(/\t+/, ''),
+		body: `It isn't easy when a dot-com as reputable and beloved as the one and only pointless dot-com falls short of its dedicated patrons' expecations. *And indeed*, we have fallen short. As you can plainly see, our last public update was a whopping **eight months ago** when we promised semi-fungible red dot tokens. "And where are they?" you ask.
+
+		And boy, are you ever right to ask. But boy, do we ever have some answer.
+
+		You see, it all started six months ago. We had millions of these virtual red dots just laying around, awaiting token fungitization. And we said, "Hey, let's fungitize these dots." But see, that's where things got hairy &mdash; really, literally hairy.
+
+		For reasons unbeknownst to me, the dots started sprouting beard hairs. You can't even make this stuff up. They grew giant beards, formed a biker gang, and literally rode off. Red dots, which you normally expect to be doing literally nothing ([at least around here](/reddot)), grew beards, and rode into the freaking sunset.
+
+		It was majestic. It was a little scary. And in the end, it's actually pretty infuriating. We had big plans for these dots, their tokenization, their fungitization, etc.. They were going to be famous. And now where are they?
+
+		Freaking sunset. On motorcycles. Beards and everything.
+
+		So, that's what we're up against here. Sure, there are still some dots around. But, not as many &mdash; what with all the dots in the sunset now with their beards. But, rest assured, we haven't forgotten about our promise. If push comes to shove, we'll see about coaxing them back with some sweet new rides or something. Or perhaps beard trimmers.
+
+		In the meantime, sit tight. We'll get those fungible red dot tokens. We're just dealing with quite a lot of unexpected drama at the moment.
+
+		Thanks for your patience.
+		`.replace(/\t+/g, ''),
+		pubDate: new Date("2024-02-25").toGMTString(),
+		guid: 'e860b3e4-8789-4e3d-b2c2-ccf09a9c172b',
+	},
+	{
 		title: "Red Dot Fungible Token?",
-		link: "https://www.thepointless.com/",
+		link: "https://www.thepointless.com/#red-dot-fungible-token",
 		description: "It has been suggested that thepointless.com launch its very own Red Dot Fungible Token. We don't necessarily disagree. But, what does that mean? Read more at thepointless.com.",
 		body: `It has been suggested that thepointless.com launch
 			<b>its very own</b> <a href="https://github.com/svidgen/www.thepointless.com/issues/12">Red Dot Fungible Token</a>.
 			We do not necessarily disagree. That is why we are beginning to look very
 			seriously at the possibility of a <b>semi-fungible red dot token</b>.
-			We promise it will be awsome and highly valuable. Please watch our RSS feed for more information in the coming months.`,
-		pubDate: new Date("2023-07-10").toGMTString()
+			We promise it will be awsome and highly valuable. Please watch our RSS feed for more information in the coming months.`.replace(/\t+/, ''),
+		pubDate: new Date("2023-07-10").toGMTString(),
+		guid: '890c82c3-7db4-4da9-a4ee-3a8f3508a581',
 	}
 ];
 
